bcc4245d520bfdfa4cc8465b1cdcaf39ef12bc6f
arch linux <hoangsonj4f@gmail.com>
2024-04-03 12:36:33 +0700

    fix auth error

--- src/auth.ts
+++ src/auth.ts
@@ -8,99 +8,97 @@ import { CleaningDelay, Statuses } from '@prisma/client'
 import NextAuth from 'next-auth'
 
 export const {
-	handlers: { GET, POST },
-	auth,
-	signIn,
-	signOut,
-	unstable_update,
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+  unstable_update,
 } = NextAuth({
-	pages: {
-		signIn: '/auth/login',
-		error: '/auth/error',
-	},
-	events: {
-		async linkAccount({ user }) {
-			await db.user.update({
-				where: { id: user.id },
-				data: {
-					emailVerified: new Date(),
-				},
-			})
-		},
-	},
-	callbacks: {
-		async signIn({ user, account }) {
-			if (account?.provider !== 'credentials') {
-				return true
-			}
+  trustHost: true,
+  pages: {
+    signIn: '/auth/login',
+    error: '/auth/error',
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: {
+          emailVerified: new Date(),
+        },
+      })
+    },
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider !== 'credentials') {
+        return true
+      }
 
-			if (!user?.id) {
-				return false
-			}
+      if (!user?.id) {
+        return false
+      }
 
-			const existingUser = await getUserById(user.id)
+      const existingUser = await getUserById(user.id)
 
-			if (!existingUser?.emailVerified) return false
+      if (!existingUser?.emailVerified) return false
 
-			if (existingUser.isTwoFactorEnabled) {
-				const twoFactorConfirmation =
-					await getTwoFactorConfirmationByUserId(
-						existingUser.id,
-					)
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id
+        )
 
-				if (!twoFactorConfirmation) return false
+        if (!twoFactorConfirmation) return false
 
-				// Delete two factor confirmation for next sign in
-				await db.twoFactorConfirmation.delete({
-					where: { id: twoFactorConfirmation.id },
-				})
-			}
-			return true
-		},
-		async session({ session, token }) {
-			if (session.user && token.sub) {
-				session.user.id = token.sub
-			}
+        // Delete two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: { id: twoFactorConfirmation.id },
+        })
+      }
+      return true
+    },
+    async session({ session, token }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub
+      }
 
-			if (session.user) {
-				session.user.name = token.name
-				session.user.image = token.image as string
-				session.user.isTwoFactorEnabled = token
-					.isTwoFactorEnabled as boolean
-				session.user.email = token.email as string
-				session.user.isOAuth = token.isOAuth as boolean
-				session.user.status = token.status as Statuses
-				session.user.bio = token.bio as string
-				session.user.cleaningDelay = token
-					.cleaningDelay as CleaningDelay
-				session.user.createdAt = token.createdAt as Date
-			}
+      if (session.user) {
+        session.user.name = token.name
+        session.user.image = token.image as string
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean
+        session.user.email = token.email as string
+        session.user.isOAuth = token.isOAuth as boolean
+        session.user.status = token.status as Statuses
+        session.user.bio = token.bio as string
+        session.user.cleaningDelay = token.cleaningDelay as CleaningDelay
+        session.user.createdAt = token.createdAt as Date
+      }
 
-			return session
-		},
-		async jwt({ token }) {
-			if (!token.sub) return token
+      return session
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token
 
-			const existingUser = await getUserById(token.sub)
+      const existingUser = await getUserById(token.sub)
 
-			if (!existingUser) return token
+      if (!existingUser) return token
 
-			const existingAccount = await getAccountByUserId(existingUser.id)
+      const existingAccount = await getAccountByUserId(existingUser.id)
 
-			token.isOAuth = !!existingAccount
-			token.name = existingUser.name
-			token.image = existingUser.image
-			token.email = existingUser.email
-			token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled
-			token.status = existingUser.status
-			token.bio = existingUser.bio || ''
-			token.cleaningDelay = existingUser.cleaningDelay
-			token.createdAt = existingUser.createdAt
+      token.isOAuth = !!existingAccount
+      token.name = existingUser.name
+      token.image = existingUser.image
+      token.email = existingUser.email
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled
+      token.status = existingUser.status
+      token.bio = existingUser.bio || ''
+      token.cleaningDelay = existingUser.cleaningDelay
+      token.createdAt = existingUser.createdAt
 
-			return token
-		},
-	},
-	adapter: PrismaAdapter(db),
-	session: { strategy: 'jwt' },
-	...authConfig,
+      return token
+    },
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: 'jwt' },
+  ...authConfig,
 })
